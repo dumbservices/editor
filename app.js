bdb64398f06913028d80fab9fd9bbe3c47933ad6
fitsum <fitsumbelay@gmail.com>
2023-09-22 17:50:09 -0400

    set default content for editor + add helper function

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const $ = sel => document.querySelector( sel )
 const editor = $( '.editor textarea' )
 const viewport = $( '.viewport' )
 
+const initEditorCode = '<div style="background:red;padding:10px;color: white;font-family:sans-serif">hello world</div>'
+
+const contentify = function ( target, content ) { target.innerHTML = content }
+
 const handleEditorInput = e => {
-  const content = e.target.value
+  contentify( viewport, e.target.value )
   /* const domified = new DOMParser().parseFromString( content, 'text/html' ) */
-  viewport.innerHTML = content
-  console.log( { content } )
 }
 
 editor.addEventListener( 'input', handleEditorInput )
 
 const init = () => {
   editor.focus()
+  editor.value = initEditorCode
+  contentify( viewport, initEditorCode )
 }
 
 document.addEventListener( 'DOMContentLoaded', init )
